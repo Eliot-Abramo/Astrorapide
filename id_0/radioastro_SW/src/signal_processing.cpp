f6c58f9b41549db03e7ba80d1a504f3371275b6b
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-16 00:34:13 +0200

    adapted constants

--- id_0/radioastro_SW/src/signal_processing.cpp
+++ id_0/radioastro_SW/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de4a3e42bb0a81d16772d71afaa40087aca75aa96c7957f605fad2b79bec61b8
-size 24030
+oid sha256:1565d124e82957c4e037b3d67ef5a551ee8bd81e8ceddb3ac5124c604a6ba886
+size 23640
--- id_0/radioastro_SW/src/signal_processing.h
+++ id_0/radioastro_SW/src/signal_processing.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b84b140805d065f896f371604ee1d7f069aa80420333120e9ce69bc19dcf355
-size 1895
+oid sha256:5ddaa037cf16fc7725d0ff36b77b7edae63a13432633f06ed51dd3d85a78e4c8
+size 2294
