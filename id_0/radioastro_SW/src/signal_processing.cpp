405e3b25405474e704e54d32f6b787b4776df904
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-15 22:00:11 +0200

    first attempt at reducing overhead

--- id_0/radioastro_SW/src/signal_processing.cpp
+++ id_0/radioastro_SW/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3966d5695c36b3e923ff9e0fcaa0f35ab9d78e9bd50d359ed91850a4872b2f9
-size 15159
+oid sha256:5046c1087e70fc5de134a57b695903b63b8b20fef5877a3006ec9a4e968a2e5a
+size 16484
