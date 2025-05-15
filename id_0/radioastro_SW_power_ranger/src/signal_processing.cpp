a61a79bdf2b39aac63de0762b1b983156a114d3b
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-15 22:29:15 +0200

    second attempt with OpenMP

--- id_0/radioastro_SW_power_ranger/src/signal_processing.cpp
+++ id_0/radioastro_SW_power_ranger/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2226c65670594d4b12506083fb49a67f4198efc3fce98bebe7b2dc02d34183a7
-size 12924
+oid sha256:351318639cce36b31bc7ee312df3a8a20325603e1fc533cb5a79679bfe6919fc
+size 13788
