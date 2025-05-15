0cdc7d3962e73832fddd37efa93a83adc5556604
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-15 23:26:11 +0200

    trying to get some accuracy back

--- id_0/radioastro_SW/src/signal_processing.cpp
+++ id_0/radioastro_SW/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddef88fcf4b40e3ce1f6eeafc05a839a3343c768095affe95ab7d901889575e2
-size 31846
+oid sha256:e0eeb71464c5ccaed2417784911ad18fbe4422280731ba0785fe591ece410b06
+size 33830
