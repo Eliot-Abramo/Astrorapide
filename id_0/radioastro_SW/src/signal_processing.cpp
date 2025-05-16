9b633b17d7da5a4f0b90a331aaaab64489a5ca59
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-16 03:47:43 +0200

    peak average

--- id_0/radioastro_SW/src/signal_processing.cpp
+++ id_0/radioastro_SW/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:780a54b429496f092e1c0213efd53c6f44301f2373a833d67320d42c83d124e7
-size 23530
+oid sha256:8fa6a7832ca04c1c9a8b068dc291f641cf2dd41a7aa2cd60c30a06ab10c569ad
+size 25203
