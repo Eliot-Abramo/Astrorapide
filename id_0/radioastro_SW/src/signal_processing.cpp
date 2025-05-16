3400878ee0130a1bf51d48e29ba34ab58edfef96
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-16 03:45:50 +0200

    peak smoothing tests and average

--- id_0/radioastro_SW/src/signal_processing.cpp
+++ id_0/radioastro_SW/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdd18191d4c9180baaf454f08f82f5a4604f4dad839ed5b80edd91c0ed8514a3
-size 19122
+oid sha256:780a54b429496f092e1c0213efd53c6f44301f2373a833d67320d42c83d124e7
+size 23530
