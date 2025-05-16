d83b759117c7094a07e24f0e4fb8845168b03843
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-16 14:16:44 +0200

    attempting error rectification

--- id_0/radioastro_SW/src/signal_processing.cpp
+++ id_0/radioastro_SW/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fa6a7832ca04c1c9a8b068dc291f641cf2dd41a7aa2cd60c30a06ab10c569ad
-size 25203
+oid sha256:ac4ce88c194ea2674edb5ef7367b3accfc2b3e89081428ae067211f2e0c51e66
+size 14743
