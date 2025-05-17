fb9fcfd67b85f9bd3c22607a529d6240a989fd7a
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-18 01:31:23 +0200

    cleanup for sol5

--- extractBitstream.sh
+++ extractBitstream.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac3c68432f369e81a62a7888ed0c03099ed97b017887b290349b10b0d42f04a1
-size 189
+oid sha256:26770fea99ee50c68f7207c71c81a1f580b6f04c921e1de50eae30a5c8da8439
+size 161
--- sol5/Project_Files/build_Vivado_project.sh
+++
@@ -1,2 +0,0 @@
-/tools/Xilinx/Vivado/2022.2/bin/vivado -mode batch -source recreate_xadc.tcl -tclargs --ADC
-
