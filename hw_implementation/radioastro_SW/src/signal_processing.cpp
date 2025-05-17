a297d93fc21c3596b0ac8e347175229c9c346907
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-17 18:30:13 +0200

    hail mary p3

--- hw_implementation/radioastro_SW/src/signal_processing.cpp
+++ hw_implementation/radioastro_SW/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f9f1e509f0bab203511db14d93983e21ba24725e4c212a7e1643206cf82e8c8
-size 24543
+oid sha256:0039081fe738148e21f46eacaef6fc2ec75368bcf5ef446de6a01d12a88af304
+size 24854
