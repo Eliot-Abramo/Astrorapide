d065ec685a1c60280a51d119075844b7e6866af0
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-17 14:54:17 +0200

    i

--- hw_implementation/radioastro_SW/src/signal_processing.cpp
+++ hw_implementation/radioastro_SW/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d73e5fa33b2ac3cff9973eb348c736558d8030196a5d4c296c65334a86b940e6
-size 13236
+oid sha256:af013e6e14b253b8a0bf4e839b3194506ee9c5a58c0d5897a35b0dea09f9e62e
+size 13267
