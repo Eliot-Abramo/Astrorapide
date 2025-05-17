5201e68c1fe881dc1b00f99fb40328eeabd10771
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-17 11:20:05 +0200

    modified run all

--- id_0/radioastro_SW/run_all.sh
+++ id_0/radioastro_SW/run_all.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb12837a07253cfd9dad239d3d8609ef194eb0ffe4a547d4f76a43c5ca476461
-size 920
+oid sha256:43db48c13017d2e3d8b7242a1e1dba4922f193d09b99f88281026b7bff7b1f5b
+size 873
--- id_0/radioastro_SW_power_ranger/run_all.sh
+++ id_0/radioastro_SW_power_ranger/run_all.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5b8f4f3d2dcd8687e6323ed98303a9f57d8a235b59656dd03a278aeb33d436d
-size 998
+oid sha256:834936e3bf322c05c4af29cc8ff10e76b981dd1a115a9b674d39251e6fafd91d
+size 951
