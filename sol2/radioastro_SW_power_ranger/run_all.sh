eae7df9d4a282331ee9d21febf9589a671eb7d43
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-17 11:50:32 +0200

    updated makefile and run all

--- sol2/radioastro_SW_power_ranger/run_all.sh
+++ sol2/radioastro_SW_power_ranger/run_all.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c132395ed94b76dc2f231932cacc89a09b09103871d4f2f786635aa95c9b58c
-size 955
+oid sha256:834936e3bf322c05c4af29cc8ff10e76b981dd1a115a9b674d39251e6fafd91d
+size 951
