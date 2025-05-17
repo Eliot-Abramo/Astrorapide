a524159338a9f9c9a5f3937fe4fe26546f14e415
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-17 23:42:36 +0200

    adc attempt maison

--- hw_implementation/radioastro_SW/src/signal_processing.cpp
+++ hw_implementation/radioastro_SW/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a188f1b560e8c07a9f257cb4bfafbd6df62d629cd1ffe78c2e3cde3968642d21
-size 19114
+oid sha256:e2b02baaf1d089c9c8f3a340b914b0ab310b8a75704ae8c18f1c30660b67abd4
+size 12456
