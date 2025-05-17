5b08b04c79faa254f097f0b4f804cbb723320375
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-17 14:20:15 +0200

    modifs param HW FFT

--- hw_implementation/radioastro_SW/src/signal_processing.cpp
+++ hw_implementation/radioastro_SW/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b4d83ad752771aeb77af8f81ebc5029c4842f2dc87a194a2f8070ea8e94386e
-size 13271
+oid sha256:6f82f2957c2b7d605098f7e7e857f6964e763bad01090024850b7c3b101d9faa
+size 13277
