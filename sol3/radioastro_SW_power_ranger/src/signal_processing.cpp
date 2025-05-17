5256161b5e81067eb30bc7dc2604a82c95470b05
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-17 18:20:34 +0200

    stage counting

--- sol3/radioastro_SW_power_ranger/src/signal_processing.cpp
+++ sol3/radioastro_SW_power_ranger/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22bcf07b268b3affa77910a3eb37c957ae915cb936b195804ab4ac7e46f2e6f1
-size 13789
+oid sha256:241c7fe9053605d1ba037fa945ae44f7e3a8756ac1c66a322e697b690ed7c70c
+size 14082
