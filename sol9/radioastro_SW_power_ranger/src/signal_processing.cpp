6c401a22b4341c5995f399156350cc5980bde6c3
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-18 03:09:08 +0200

    attempt cache hanning window

--- sol9/radioastro_SW_power_ranger/src/signal_processing.cpp
+++ sol9/radioastro_SW_power_ranger/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c133cc4ba19cfbedec8e298d2a82b1c112532d939d7ac89471da272989580d81
-size 16914
+oid sha256:0e955c62c2f699690e22c4cbecfd365c4cc5312af59627ce594d7ea46080f048
+size 18064
