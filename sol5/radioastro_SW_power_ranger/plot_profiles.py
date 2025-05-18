9600d2262121747e3b294fec2074a9a2fc59a49e
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-18 03:43:09 +0200

    final push sol5,6,7

--- sol5/radioastro_SW_power_ranger/plot_profiles.py
+++ sol5/radioastro_SW_power_ranger/plot_profiles.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd6843bc8fd445a3bd05b88b21c8469f166b997f9179459aab84de7d49edf3b8
-size 4614
+oid sha256:534303de338ca081e084fb65b84c2a15614ce834f5db7a1ab7fe052bd6eba3d1
+size 2132
