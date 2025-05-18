4b9b8e891f80ba6015d0f69f1d3175f5c8598159
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-18 02:20:27 +0200

    modifs

--- sol8/radioastro_SW_power_ranger/src/signal_processing.cpp
+++ sol8/radioastro_SW_power_ranger/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1734049c917060daf9ec6c3eb0f7e13afe38997fd49d3f2f0dd9c5a03c2273f8
-size 18768
+oid sha256:6c7ad444519775f40d0efa54e362749f185aa5434ac0926b675943a2ddee1b52
+size 23239
