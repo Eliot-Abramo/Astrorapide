eb15c32034eadea2921377a76d1a253a3263b105
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-18 02:47:39 +0200

    attempting sinc approx, new sol4 .bit

---
+++ sol4/sol4.tcl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2f129a9af6313e0c217900bb173593eb63f49a26a363ae18c900ae840dbfe474
+size 101124
--- sol8/radioastro_SW_power_ranger/src/signal_processing.cpp
+++ sol8/radioastro_SW_power_ranger/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f988a8646214bfbf27038b5364ef5991b95c97c38e9c746e7ded6a89193a7f3
+oid sha256:6f171c26eb793228f3df23dc7e7a9e48f8b1ec6d7e24ee08d92c73f528023cb5
 size 16037
