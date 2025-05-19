dea1e3f1a6ae8303ecfa0c80ca0013a29ccecca3
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-19 22:27:14 +0200

    sol10

--- sol10/radioastro_SW_power_ranger/src/signal_processing.cpp
+++ sol10/radioastro_SW_power_ranger/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65dfb037b1bb77a18994d1343ad8662ab9a7e35b3c8359e3ce25c89f4992fea5
-size 11636
+oid sha256:1560323e12e8c6e1eccbb4646cc302042cf727435624f6e1617cedd7c78e0518
+size 11632
