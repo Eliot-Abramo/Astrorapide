c9e1fbc35b9b8ec87d06fdfe2ddfe3c493d00b0b
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-21 00:30:28 +0200

    modified make

--- sol7 copy/radioastro_SW_power_ranger/src/testbench.cpp
+++ sol7 copy/radioastro_SW_power_ranger/src/testbench.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4cf9014c77e9e5bbbefd0fd6c1644026924c7e3d130173096aad88399c55bf3
-size 13616
+oid sha256:103729bcf82f9294322bad5088ea3508a353548a928bf1a639a0a492cb094591
+size 13580
