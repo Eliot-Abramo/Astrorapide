f405912077d60effcf607e0b4021a532cc37dea6
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-22 01:01:22 +0200

    added debug prints

--- sol11_fixed/radioastro_SW_power_ranger/src/fxp_utils.h
+++ sol11_fixed/radioastro_SW_power_ranger/src/fxp_utils.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e553957c85c7661d79cdd97efe87e00df002fc69b5f656ffe3959a01424e7c2b
-size 1512
+oid sha256:42d518866b0af45f161519074ee9e80985de33be4cebe01ca64196ab7b3f2ea4
+size 1554
--- sol11_fixed/radioastro_SW_power_ranger/src/signal_processing.cpp
+++ sol11_fixed/radioastro_SW_power_ranger/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7746393bf8405e519b7d14a2d24dc1e9202f605eb43e1110b2a9e442b51a309c
-size 14305
+oid sha256:05c1b0b9c616cb07552849ad0ca204ec703d5bb9992189faa3b9d232d4a48f5c
+size 14895
