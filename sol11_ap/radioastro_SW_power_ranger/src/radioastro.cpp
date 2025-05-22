e2336a1c3f10b8b3c9aa41a26ee6a1a8dc90afaa
Eliot Abramo <eliot.abramo@epfl.ch>
2025-05-22 03:17:01 +0200

    attempted optimizations

--- sol11_ap/radioastro_SW_power_ranger/src/radioastro.cpp
+++ sol11_ap/radioastro_SW_power_ranger/src/radioastro.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5098110951e486155c98f88ce67f60fdf4abb973adfc93a1eb87b95b1e89de01
-size 13545
+oid sha256:5106b3b79c79e975aec34ae06bc822ff829e3929d69168c7b8fdff8c24affccd
+size 13234
--- sol11_ap/radioastro_SW_power_ranger/src/signal_processing.cpp
+++ sol11_ap/radioastro_SW_power_ranger/src/signal_processing.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb478e15a0dd8aba36177810aa1a69e539006fb9869629c7c3711fef883a3c24
-size 14963
+oid sha256:dee1fd99e62ae4667aa3aab327cbe8ca5330a5ffffcddcbbb84d00b5db1da2e9
+size 14035
